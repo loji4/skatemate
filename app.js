5b04aee1351ac204a7f4c46cb521ba9500e19915
loji4 <yogawithlindao@gmail.com>
2021-09-06 18:32:30 +1000

    modified buttons

---
+++ app.js
@@ -0,0 +1,6 @@
+document.querySelector('.js-btn').addEventListener('click',btnFunc)
+
+function btnFunc (e) {
+   let btn = document.querySelector('.js-btn')
+   btn.style.backgroundColor = '#ADD8E6'
+}
