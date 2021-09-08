b39dc446a40c54e9139189c3085735a05b232755
loji4 <yogawithlindao@gmail.com>
2021-09-08 13:44:40 +1000

    added js for btn 1

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ function btnFunc (e) {
    let btn = document.querySelector('.js-btn')
    btn.style.backgroundColor = '#ADD8E6'
 }
+
+getReco()
+
+function getReco (e) {
+let button1 = document.querySelectorAll('#btncheck1');
+let button2 = document.querySelectorAll('#btncheck2');
+ if (button1) {
+        button1.innerHTML ='you clicked me'
+    }
+}
+
+getReco()
